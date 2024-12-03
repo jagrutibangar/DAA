969a5b1954ee6712539bb15974ec31d6c5bf22f8
jagrutibangar <161784351+jagrutibangar@users.noreply.github.com>
2024-12-03 15:23:05 +0530

    Practical04A.cpp

--- Practical04A.cpp
+++ Practical04A.cpp
@@ -1,34 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <set>
-#include <algorithm>
 using namespace std;
 
 void findSubsets(vector<int>& nums) {
     int n = nums.size();
-    set<vector<int>> uniqueSubsets; // To avoid duplicates
-    
-    // Sort the array to ensure subsets are in order
-    sort(nums.begin(), nums.end());
-    
-    // Generate all subsets using bit masking
+
+    // Sort the array manually to handle duplicates
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (nums[j] > nums[j + 1]) {
+                swap(nums[j], nums[j + 1]);
+            }
+        }
+    }
+
+    cout << "{ ";
     for (int mask = 0; mask < (1 << n); mask++) {
         vector<int> subset;
+        bool skip = false;
+
         for (int i = 0; i < n; i++) {
-            // Check if the i-th bit is set in the mask
+            // Check if the i-th bit is set
             if (mask & (1 << i)) {
+                // Skip duplicates in the same subset
+                if (i > 0 && (mask & (1 << (i - 1))) == 0 && nums[i] == nums[i - 1]) {
+                    skip = true;
+                    break;
+                }
                 subset.push_back(nums[i]);
             }
         }
-        uniqueSubsets.insert(subset);
-    }
-    
-    // Print all unique subsets
-    cout << "{ ";
-    for (auto subset : uniqueSubsets) {
-        cout << "{";
-        for (int val : subset) cout << val << " ";
-        cout << "} ";
+
+        if (!skip) {
+            cout << "{ ";
+            for (int val : subset) cout << val << " ";
+            cout << "} ";
+        }
     }
     cout << "}" << endl;
 }
